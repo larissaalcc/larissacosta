3c4cd93ff3d3c2acaaaf2df39d8e662818b3b684
larissa <larissacgr05@gmail.com>
2023-06-05 21:00:42 -0400

    update

--- tratamento.py/ex_1.py
+++ tratamento.py/ex_1.py
@@ -3,6 +3,7 @@ lista_cliente=[]
 lista_passagem=[]
 lista_aviao=[]
 lista_tripulacao=[]
+c=0
 while True:
     menu= int(input(" \nDigite:\n 1 Para Cadastro do cliente\n 2 Para Cadastro de passagem\n 3 Para Cadastro do avião\n 4 Para Cadastro da tripulação\n 5 Para Imprimir\n 6 Para sair\n Sua opção:"))
     if menu == 1:
@@ -22,57 +23,101 @@ while True:
                 idade=int(input("\nIdade: "))
             except:
                 print("Por favor, digite apenas números. Tente novamente")
-            else:
-                lista_cliente.append(nome,sobrenome,rg,cpf,fone,idade)
+            lista_cliente.append(nome)
+            lista_cliente.append(sobrenome)
+            lista_cliente.append(rg)
+            lista_cliente.append(cpf)
+            lista_cliente.append(fone)
+            lista_cliente.append(idade)
+            break       
     if menu == 2:
         print("----BEM VINDO AO CADASTRO DE PASSAGENS----")
-        
         try:
             destino=str(input("\nSeu destino: "))
             origem=str(input("\nSua origem: "))
             dias=int(input("\nDuração em dias:"))
-            v=float(input("\nValor da passagem: "))
-            d=(5/100*v)
-            print("\nDesconto 5%:{:.2f}".format(d))
-            t=(v-d)
-            print("Total da viagem= R$",t)
+            valor=float(input("\nValor da passagem: "))
+            desconto=(5/100*valor)
+            print("\nDesconto 5%:{:.2f}".format(desconto))
+            total=(valor-desconto)
+            print("Total da viagem= R$",total)
         except:
             print("Por favor, digite apenas números. Tente novamente")
-        else:
-            lista_passagem.append(destino,origem,dias,v,d,t)
+        lista_passagem.append(destino)
+        lista_passagem.append(origem)
+        lista_passagem.append(dias)
+        lista_passagem.append(valor)
+        lista_passagem.append(desconto)
+        lista_passagem.append(total)
+        continue
     if menu == 3:
         print("----BEM VINDO AO CADASTRO DO AVIÃO----")
         try:
            modelo=int(input("\nModelo da Aeronave: "))
            ano=int(input("\nAno de fabricação: "))
-           horas=int(input("Horas de voo: "))
-           cor=str(input("Cor da Aeronave: "))
-           passageiros=int(input("Quantidade de passageiros:"))
+           horas=int(input("\nHoras de voo: "))
+           cor=str(input("\nCor da Aeronave: "))
+           passageiros=int(input("\nQuantidade de passageiros:"))
         except:
             print("Por favor, digite apenas números. Tente novamente")
-        else:
-            lista_aviao.append(modelo,ano,horas,cor,passageiros)
+        lista_aviao.append(modelo)
+        lista_aviao.append(ano)
+        lista_aviao.append(horas)
+        lista_aviao.append(cor)
+        lista_aviao.append(passageiros)
+        continue
     if menu == 4:
         print("----BEM VINDO AO CADASTRO DA TRIPULAÇÃO---")
         try:
             nome=str(input("\nNome: "))
-            cargo=str(input("Descrição do cargo: "))
+            cargo=str(input("\nDescrição do cargo: "))
         except:   
             print("Digite apenas letras")
         try:
             idadeTripulacao=int(input("\nIdade: "))
-            admissao=int(input("Data de Admissão: "))
-            contato=int(input("Fone para contato: "))
+            admissao=int(input("\nAno de Admissão: "))
+            contato=int(input("\nFone para contato: "))
         except:
             print("Por favor, digite apenas números. Tente novamente")
-        else:
-            lista_tripulacao.append(nome,cargo,idadeTripulacao,admissao,contato)
+        luiz=[nome,cargo,idadeTripulacao,admissao,contato]
+        for i in luiz:
+            lista_tripulacao.append(i)
+            continue
     if menu == 5:
-        print("\nRELATÓRIOS")
-        print(lista_cliente)
-        print(lista_passagem)
-        print(lista_aviao)
-        print(lista_tripulacao)
+        clienteP=["Nome:","Sobrenome:","Rg:","Cpf:","Telefone:","Idade:"]
+        passagem=["Destino:","Origem:","Dias","Valor","Desconto:","Total:"]
+        aviao=["Modelo:","Ano:","Horas","Cor","Passageiros:"]
+        tripulacao=["Nome:","Descrição do cargo:","Idade:","Data de admissão:","Telefone para contato:"]
+        print("\nRELATÓRIO CLIENTE")
+        c=0
+        if len(lista_cliente)>0:
+            for pergunta in clienteP:
+            
+                print(pergunta,lista_cliente[c])
+                c+=1
+        c=0
+        print("-"*100)
+        print("PASSAGEM")
+        if len(lista_passagem)>0:
+            for pergunta in passagem:
+                print(pergunta,lista_passagem[c])
+                c+=1
+        c=0
+        print("-"*100)
+        print("AERONAVE")
+        if len(lista_aviao)>0:
+            for pergunta in aviao:
+                print(pergunta,lista_aviao[c])
+                c+=1
+        c=0
+        print("-"*100)
+        print("TRIPULAÇÃO")
+        if len(lista_tripulacao)>0:
+            for pergunta in tripulacao:
+                print(pergunta,lista_tripulacao[c])
+                c+=1
+        c=0
+        continue
     elif menu == 6:
         break
 
