3a8342193b330ce4f6383df21699b23cc7fe4f03
Gabriel Gravena Barros <larissacgr05@gmail.com>
2023-06-12 18:24:38 -0300

    update ex3

--- tratamento.py/dicionario.py/ex3.py
+++ tratamento.py/dicionario.py/ex3.py
@@ -1,18 +1,10 @@
-listaCpf=[]
-listaNome=[]
-listaIdade=[]
-listaTelefone=[]
+dicionario={}
 while True:
     cpf=(input("Seu cpf ou 0 para imprimir: "))
     if cpf == "0":
-        print(dicionario)
         break
     nome=input("Digite seu nome: ")
     idade=(input("Sua idade: "))
     telefone=(input("Telefone para contato: "))
-    listaCpf.append(cpf)
-    listaNome.append(nome)
-    listaIdade.append(idade)
-    listaTelefone.append(telefone)
-    dicionario={"cpf",cpf,"Nome:",nome,"Idade:",idade,"Telefone:",telefone}
-   
+    dicionario[cpf]={"Nome: ":nome,"Idade: ":idade,"Telefone:":telefone}
+print(dicionario)  
