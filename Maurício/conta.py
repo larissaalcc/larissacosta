c336ac593bfb34b8ee6278c29320ac6cf5dae246
Haave ( Gleison ) <132004882+larissaalcc@users.noreply.github.com>
2023-08-25 14:53:54 -0400

    update

--- Maurício/conta.py
+++ Maurício/conta.py
@@ -1,5 +1,7 @@
-from PySide6.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QLineEdit, QBoxLayout, QDialog, QFrame
-import sys 
-from contabancaria_ui import Ui_MainWindow 
+class contabancaria():
+    def __init__(self):
+        
 
-class 
+    
+        
+        
