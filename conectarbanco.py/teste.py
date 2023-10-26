d221b381bcb69f1546b006db7ae8a66f76c35107
Gabriel Gravena Barros <larissacgr05@gmail.com>
2023-10-26 17:45:19 -0300

    update teste

--- conectarbanco.py/teste.py
+++ conectarbanco.py/teste.py
@@ -1,13 +1,13 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
+meubanco = mysql.connector.connect(
   host="10.28.1.198",
   user="suporte",
   password="suporte",
   database="projeto"
 )
 
-mycursor = mydb.cursor()
+meucursor = meubanco.cursor()
 
 # Operações CRUD
 
@@ -15,14 +15,14 @@ mycursor = mydb.cursor()
 def create_data(name, age, email):
     sql = "INSERT INTO customers (name, age, email) VALUES (%s, %s, %s)"
     val = (name, age, email)
-    mycursor.execute(sql, val)
-    mydb.commit()
-    print(mycursor.rowcount, "registro(s) inserido(s).")
+    meucursor.execute(sql, val)
+    meubanco.commit()
+    print(meucursor.rowcount, "registro(s) inserido(s).")
 
 # READ
 def read_data():
-    mycursor.execute("SELECT * FROM customers")
-    myresult = mycursor.fetchall()
+    meucursor.execute("SELECT * FROM customers")
+    myresult = meucursor.fetchall()
     for x in myresult:
         print(x)
 
@@ -30,20 +30,21 @@ def read_data():
 def update_data(id, name, age, email):
     sql = "UPDATE customers SET name = %s, age = %s, email = %s WHERE id = %s"
     val = (name, age, email, id)
-    mycursor.execute(sql, val)
-    mydb.commit()
-    print(mycursor.rowcount, "registro(s) atualizado(s).")
+    meucursor.execute(sql, val)
+    meubanco.commit()
+    print(meucursor.rowcount, "registro(s) atualizado(s).")
 
 # DELETE
 def delete_data(id):
     sql = "DELETE FROM customers WHERE id = %s"
     val = (id,)
-    mycursor.execute(sql, val)
-    mydb.commit()
-    print(mycursor.rowcount, "registro(s) excluído(s).")
+    meucursor.execute(sql, val)
+    meubanco.commit()
+    print(meucursor.rowcount, "registro(s) excluído(s).")
 
 
 create_data("Larissa", 32, "mauricio1desouza@gmail.com")
 create_data( "luiz", 30, "thiago.almeida@example.com")
 
-mydb.close()
+meubanco.close()
+,
